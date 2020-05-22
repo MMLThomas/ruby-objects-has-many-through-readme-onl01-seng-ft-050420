20357b7a7242923cffef3225e772d4301a9fd8c4
michael thomas <michael.thomas2014@gmail.com>
2020-05-22 04:48:07 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -18,6 +18,20 @@ class Waiter
     Meal.new(self, customer, total, tip)
   end
   
+  def meals
+    Meal.all.select{|meal| meal.waiter == self}
+  end
+  
+  def best_tipper
+    best_tip = 0
+    best_tipper = nil
+    meals.each do |meal| 
+      if meal.tip > best_tip
+        best_tipper = meal.customer
+      end
+    end
+    best_tipper
+  end
   
 
 end
