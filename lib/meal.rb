6b2a0325c57121f8f0077a447fbc728c7015de15
michael thomas <michael.thomas2014@gmail.com>
2020-05-22 04:24:22 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
+  attr_accessor :tip, :customer, :waiter, :total
+  attr_reader :name, 
+  
+  @@all = []
+  
+  def initialize(waiter, customer, total, tip = 0)
+    @total = total
+    @tip = tip
+    @customer = customer
+    @waiter = waiter
+    @all << self
+  end
+  
+  def self.all
+    @@all
+  end
 
 end
