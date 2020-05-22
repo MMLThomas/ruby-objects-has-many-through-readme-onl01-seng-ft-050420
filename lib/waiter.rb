cc2bc636f3ea0d54490003bf918501855fbd3ccd
michael thomas <michael.thomas2014@gmail.com>
2020-05-22 04:29:12 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -2,12 +2,12 @@ class Waiter
   attr_accessor :years_of_experience
   attr_reader :name
   
-  @all = []
+  @@all = []
   
   def initialize(name, years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
-    @all << self
+    @@all << self
   end
   
   def self.all
